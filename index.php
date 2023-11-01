203795293157441700d2cfa92ad4d29946c543f2
Jeferson Siqueira <jeferson.siqueira-terceiro@cea.com.br>
2023-11-01 02:36:15 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCKTYPE html>
+<html lang="">
+
+<head>
+    <title>Sistema de Clínicas - PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div class="login">
+    <div class="form">
+        <form class="registro">
+            <input type="email" placeholder="Usuário" name="usuario">
+            <input type="password" placeholder="Senha" name="senha">
+            <button>Login</button>
+        </form>
+    </div>
+</div>
+</body>
+
+</html>
